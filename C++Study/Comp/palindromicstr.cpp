0aefb665bdc27db6f8bf2cc40f86bf4553363963
tuling56 <tuling56@gmail.com>
2015-09-23 20:11:40 +0800

    完善回文字符串的问题

--- C++Study/Comp/palindromicstr.cpp
+++ C++Study/Comp/palindromicstr.cpp
@@ -12,13 +12,13 @@ int judge_if(string input)
 	{
 		for (;j > i; j--)
 		{
-			cout << input[i] << " PK " << input[j] << endl;
+			//cout << input[i] << " PK " << input[j] << endl;
 			if (input[i]==input[j]){
 				j--;
 				break;
 			}
 			else{
-				cout << input[i] << "!=" << input[j] << endl;
+				//cout << input[i] << "!=" << input[j] << endl;
 				return 0;
 			}
 		}
@@ -26,10 +26,30 @@ int judge_if(string input)
 	return 1;
 }
 
+//判断能否插入一个字符后变成回文字符串
+//区分大小写（默认全部是小写）
+int judge_insert_if(string input)
+{
+	for (int i = 0; i < input.length();i++)
+	{
+		for (char c = 'a'; c <='z';c++)
+		{
+			string teststr = input;
+			teststr.insert(i, 1, c);
+			int jd = judge_if(teststr);
+			if (jd == 0)
+				continue;
+			else
+				return 1;
+		}
+	}
+	return 0;
+}
+
 
 int main()
 {
-	int res = judge_if("htcath");
+	int res = judge_insert_if("htcathe");
 	if (res==0){
 		cout << "no roll back" << endl;
 	}
